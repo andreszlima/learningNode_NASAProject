21e4f6c2c0f656d37e6a47fbf1c77351aa1bdb6d
andreszlima <andre.szlima1@gmail.com>
2022-08-05 21:56:55 -0300

    post

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -13,6 +15,22 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches () {
+    console.log('opamodel')
+    return Array.from(launches.values())
+}
+
+function addNewLaunch(launch){
+    latestFlightNumber = 100
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        flightNumber: latestFlightNumber,
+        costumer:  [`Zero to Mastery`, `NASA`],
+        upcoming: true,
+        success: true
+    }))
+}
+
 module.exports = {
-    launches,
+    getAllLaunches,
+    addNewLaunch,
 }
